8c96fcd101b9f8c33b6e746cce87c148ded039bc
telerunsyou-cell <telerunsyou@gmail.com>
2026-05-11 03:56:30 -0400

    Update flood.js

--- commands/flood.js
+++ commands/flood.js
@@ -1,1 +1,17 @@
+module.exports = {
+  name: "flood",
 
+  async execute(interaction) {
+    const message = interaction.options.getString("message");
+    const count = interaction.options.getInteger("count") || 1;
+
+    await interaction.reply({
+      content: "Sending messages...",
+      ephemeral: true
+    });
+
+    for (let i = 0; i < count; i++) {
+      await interaction.channel.send(`${message} [Willy]`);
+    }
+  }
+};
